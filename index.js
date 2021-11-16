ec998387ddc600d95428dcef0fceac2ac17cc1df
bellicose77 <64400354+bellicose77@users.noreply.github.com>
2021-11-17 02:51:21 +0600

    add service

--- index.js
+++ index.js
@@ -14,9 +14,22 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lt5tb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri);
 
 async function run() {
     try {
+        await client.connect();
+        console.log("database connect sucessfully");
+        const database = client.db("CarDetails");
+        const details = database.collection("details");
+
+        //Post api to send car details to database
+        app.post('/details', async (req, res) => {
+            const detail = req.body;
+            console.log("hiting the server", detail);
+            const result = await details.insertOne(detail);
+            res.json(result)
+        })
 
     }
     finally {
